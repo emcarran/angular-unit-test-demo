8a6a618e19df2cc694104e2f96a329bc1a3cb70b
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-08-23 15:13:35 -0700

    fixing the karma  conf

---
+++ app/components/helloServiceSpec.js
@@ -0,0 +1,7 @@
+describe("helloService", function () {
+    beforeEach(module('HelloModule'));
+
+    it('should return "hello" when called', inject(function (helloService) {
+        expect(helloService()).toBe("hello");
+    }));
+});
