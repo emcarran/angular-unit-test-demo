2ab25a8f5b4d1331845919e46a74900ca2301185
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-08-24 17:07:32 -0700

    fixing the karma config

--- app/components/helloService.js
+++ app/components/helloService.js
@@ -1,6 +1,12 @@
 angular.module('HelloModule', [])
-    .factory('helloService', function () {
+    .factory('uppercaseService', function () {
         return function () {
             return 'hello';
+        }
+    })
+    //When called, helloService() returns the string 'hello'
+    .factory('helloService', function (uppercaseService) {
+        return function () {
+            return uppercaseService('hello');
         }
     });
--- app/components/helloServiceSpec.js
+++ app/components/helloServiceSpec.js
@@ -1,7 +1,16 @@
 describe("helloService", function () {
     beforeEach(module('HelloModule'));
 
-    it('should return "hello" when called', inject(function (helloService) {
-        expect(helloService()).toBe("hello");
-    }));
+    //inject helloService into the spec code by making a call to inject(). Call inject directly in the spec function
+    it('should return "hello" when called', function () {
+        module(function ($provide) {
+            //use the build in $provide service to mock the upperCaseService, simply return the value that is passed into upperCaseService w/o modifying it
+            $provide.value('uppercaseService', function (value) {
+                return value;
+            });
+        });
+        inject(function (helloService) {
+            expect(helloService()).toBe("hello");
+        });
+    });
 });
